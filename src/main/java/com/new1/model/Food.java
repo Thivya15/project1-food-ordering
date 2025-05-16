a00fa14eaf22e27ed6e3f09a1a4431101e00924a
Thivya15 <thivyamahendran15@gmail.com>
2025-05-16 23:28:41 +0530

    write food entity

--- src/main/java/com/new1/model/Food.java
+++ src/main/java/com/new1/model/Food.java
@@ -1,9 +1,10 @@
 package com.new1.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Food {
@@ -11,4 +12,35 @@ public class Food {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Long price;
+
+    @ManyToOne
+    private Category category;
+
+    @Column(length = 1000)
+    @ElementCollection
+    private List<String> images;
+
+    private boolean available;
+
+    @ManyToOne
+    private Restaurant restaurant;
+
+    private boolean isVegetarian;
+
+    private boolean isSeasonal;
+
+    @ManyToOne
+    private List<IngredientsItem> ingredients = new ArrayList<>();
+
+    private Date creationDate;
+
+
+
+
 }
